fdc1e4cee66d8d2943a5c662a2226395cf7ae61d
nikita-2727 <provotorovnikita@gmail.com>
2024-02-27 19:09:20 +0300

    initial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+let tg = window.Telegram.WebApp
+
+
+function App() {
+    return (
+        <div className="App">
+            <p>is working</p>
+        </div>
+    );
+}
+
+export default App;
