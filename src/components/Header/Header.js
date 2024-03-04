b17a09df192ca2390e75454ccebe351605311774
nikita-2727 <provotorovnikita@gmail.com>
2024-03-04 17:16:01 +0300

    main

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { SlBasket } from "react-icons/sl";
+import './header.css'
 
 
 
@@ -7,9 +8,18 @@ import { SlBasket } from "react-icons/sl";
 class Header extends Component {
   render() {
     return (
-        <>
-            <h1 className='market-name'>React Online Store</h1>
-        </>
+        <div className='header'>
+            <div className='header-text'>
+                <h1 className='market-name'>React Online Store</h1> 
+            </div>
+            <div className='header-menu'>
+                <a className='header-menu-button'>О нас</a>
+                <a className='header-menu-button'>Отзывы</a>
+                <a className='header-menu-button'>Контакты</a>
+                <a className='cart-button'><SlBasket className='cart-icon'/></a>
+            </div>
+        </div>
+        
     )
   }
 }
