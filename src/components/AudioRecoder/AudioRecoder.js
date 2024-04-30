ebdd6b550506a363b92c0bab57278dd2ffc53ace
nikita-2727 <provotorovnikita@gmail.com>
2024-04-30 22:30:24 +0300

    aaa

--- src/components/AudioRecoder/AudioRecoder.js
+++ src/components/AudioRecoder/AudioRecoder.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import './AudioRecoder.css'
+import ButtonBack from '../Header/HeaderComponents/ButtonBack/ButtonBack.js'
 
 
 
@@ -10,11 +11,12 @@ export default function AudioRecoder (props) {
 
     return (
         <div className="video-block">
-            <p>{dataProduct.productname}</p>
+            <h2>{dataProduct.productname}</h2>
             <iframe src="https://www.youtube.com/embed/O6FPO1gMKjE?si=jsevaRfDcB6RPKdI" 
-            title="YouTube video player" frameborder="0"  className="video-frame"
+            title="YouTube video player" frameBorder="0"  className="video-frame"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+            <ButtonBack />
         </div>
     )
 }
