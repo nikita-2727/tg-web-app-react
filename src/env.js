9b5970bf585933a6332bf928287cfc838023a3af
nikita-2727 <provotorovnikita@gmail.com>
2024-07-09 12:06:47 +0300

    sdfsdf

--- src/env.js
+++ src/env.js
@@ -1,4 +1,5 @@
-const HOST_SERVER_API="http://77.222.38.197:3001/api/"
+// const HOST_SERVER_API="http://77.222.38.197:3001/api/"
+const HOST_SERVER_API ="http://localhost:3001/api/"
 // const HOST_CLIENT="http://localhost:8080"
 const HOST_CLIENT="http://localhost:3000"
 
