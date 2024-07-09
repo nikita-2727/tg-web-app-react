19fc458c1d1f6c6c06d85d41fe216b3f83b01e3e
nikita-2727 <provotorovnikita@gmail.com>
2024-07-09 11:27:50 +0300

    sdfdfdfddff

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,11 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
+import { HOST_SERVER_API } from './env';
 
 module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:3001',
+      target: HOST_SERVER_API,
       changeOrigin: true,
     })
   );
